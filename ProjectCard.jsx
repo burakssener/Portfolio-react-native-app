84d0a0c76d24b0852122146dad73fcc962336954
Burak Sener <buraksener@Buraks-MacBook-Air.local>
2025-01-12 23:39:32 +0300

    add stylesheet

--- ProjectCard.jsx
+++ ProjectCard.jsx
@@ -1,22 +1,26 @@
-import {View, Image, Text} from "react-native";
+import {View, Image, Text, StyleSheet} from "react-native";
 
 
 export default function ProjectCard({image, name}){
   return (
       <View >
         <Image source={image}
-        style={{
-          height: 150,
-          aspectRatio: 16/9,
-          borderRadius: 20,
-          marginTop: 6 
-        }}/>
-        <Text style={{
-          textAlign: "center",
-          fontSize: 16,
-          marginTop: 8
-
-        }}>{name}</Text>
+        style={styles.image}/>
+        <Text style={styles.image}>{name}</Text>
       </View>
   )   ;
 } 
+
+const styles = StyleSheet.create({
+    image: {
+        height: 150,
+        aspectRatio: 16/9,
+        borderRadius: 20,
+        marginTop: 6 
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 16,
+        marginTop: 8
+    }
+})
